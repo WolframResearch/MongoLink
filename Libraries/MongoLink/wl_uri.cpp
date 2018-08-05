4b3fecaf6cd53b0377f1f2687dcc4accc33f2cb4
Sebastian Bodenstein <sebastianb@wolfram.com>
2018-08-05 19:30:38 +0300

    refactor URI handling

--- Libraries/MongoLink/wl_uri.cpp
+++ Libraries/MongoLink/wl_uri.cpp
@@ -12,14 +12,122 @@ EXTERN_C DLLEXPORT int WL_URICreate(WolframLibraryData libData, mint Argc,
   char *uri_string = MArgument_getUTF8String(Args[1]);
 
   bson_error_t error;
-  mongoc_uri_t * uri = mongoc_uri_new_with_error(uri_string, &error);
+  mongoc_uri_t *uri = mongoc_uri_new_with_error(uri_string, &error);
   if (!uri) {
     errorString = error.message;
     libData->UTF8String_disown(uri_string);
     return LIBRARY_FUNCTION_ERROR;
   }
+
   uriHandleMap[uri_handle_key] = uri;
   // Disown string
   libData->UTF8String_disown(uri_string);
+  return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int WL_URISetOptionInt32(WolframLibraryData libData,
+                                            mint Argc, MArgument *Args,
+                                            MArgument Res) {
+  URI_GET(uri, 0)
+  char *name = MArgument_getUTF8String(Args[1]);
+  int32_t value = MArgument_getInteger(Args[2]);
+  bool res = mongoc_uri_set_option_as_int32(uri, name, value);
+  // the mongo c driver seems to report invalid option values via logging,
+  // but for some reason fails to report invalid options. Just fails.
+  // so: logging writes to error string. If its empty, report error manually
+  if (!res) {
+    if (errorString.empty())
+      errorString =
+          "Invalid option or value for \"" + std::string(name) + std::string("\"");
+    libData->UTF8String_disown(name);
+    return LIBRARY_FUNCTION_ERROR;
+  }
+  libData->UTF8String_disown(name);
+  return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int WL_URISetOptionBool(WolframLibraryData libData,
+                                           mint Argc, MArgument *Args,
+                                           MArgument Res) {
+  URI_GET(uri, 0)
+  char *name = MArgument_getUTF8String(Args[1]);
+  bool value = MArgument_getBoolean(Args[2]);
+  bool res = mongoc_uri_set_option_as_bool(uri, name, value);
+  // the mongo c driver seems to report invalid option values via logging,
+  // but for some reason fails to report invalid options. Just fails.
+  // so: logging writes to error string. If its empty, report error manually
+  if (!res) {
+    if (errorString.empty())
+      errorString =
+          "Invalid option or value for \"" + std::string(name) + std::string("\"");
+    libData->UTF8String_disown(name);
+    return LIBRARY_FUNCTION_ERROR;
+  }
+  libData->UTF8String_disown(name);
+  return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int WL_URISetOptionUTF8(WolframLibraryData libData,
+                                           mint Argc, MArgument *Args,
+                                           MArgument Res) {
+  URI_GET(uri, 0)
+  char *name = MArgument_getUTF8String(Args[1]);
+  char *value = MArgument_getUTF8String(Args[2]);
+
+  bool res = mongoc_uri_set_option_as_utf8(uri, name, value);
+  libData->UTF8String_disown(value);
+  // the mongo c driver seems to report invalid option values via logging,
+  // but for some reason fails to report invalid options. Just fails.
+  // so: logging writes to error string. If its empty, report error manually
+  if (!res) {
+    if (errorString.empty())
+      errorString =
+          "Invalid option or value for \"" + std::string(name) + std::string("\"");
+    libData->UTF8String_disown(name);
+    return LIBRARY_FUNCTION_ERROR;
+  }
+  libData->UTF8String_disown(name);
+  return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int WL_URIGetString(WolframLibraryData libData, mint Argc,
+                                       MArgument *Args, MArgument Res) {
+  URI_GET(uri, 0)
+
+  auto temp = mongoc_uri_get_options(uri);
+  MArgument_setUTF8String(Res, const_cast<char *>(mongoc_uri_get_string(uri)));
+  return LIBRARY_NO_ERROR;
+}
+
+// put named option setters together
+EXTERN_C DLLEXPORT int WL_URISetPropEnum(WolframLibraryData libData, mint Argc,
+                                         MArgument *Args, MArgument Res) {
+  URI_GET(uri, 0)
+  char *value = MArgument_getUTF8String(Args[1]);
+  mint selector = MArgument_getInteger(Args[2]);
+  bool res;
+  switch (selector) {
+  case 1:
+    res = mongoc_uri_set_auth_mechanism(uri, value);
+    break;
+  case 2:
+    res = mongoc_uri_set_auth_source(uri, value);
+    break;
+  case 3:
+    res = mongoc_uri_set_compressors(uri, value);
+    break;
+  case 4:
+    res = mongoc_uri_set_database(uri, value);
+    break;
+  case 5:
+    res = mongoc_uri_set_password(uri, value);
+    break;
+  case 6:
+    res = mongoc_uri_set_username(uri, value);
+    break;
+  }
+  libData->UTF8String_disown(value);
+  if (!res)
+    return LIBRARY_FUNCTION_ERROR;
   return LIBRARY_NO_ERROR;
 }
--- MongoLink/Kernel/URI.m
+++ MongoLink/Kernel/URI.m
@@ -21,6 +21,49 @@ uriCreate = LibraryFunctionLoad[$MongoLinkLib, "WL_URICreate",
 	"Void"
 ]
 
+uriSetOptionInt32 = LibraryFunctionLoad[$MongoLinkLib, "WL_URISetOptionInt32", 
+	{
+		Integer,					(* uri handle *)
+		"UTF8String",				(* name *)
+		Integer 					(* value *)
+	}, 
+	"Void"
+]
+
+uriSetOptionBool = LibraryFunctionLoad[$MongoLinkLib, "WL_URISetOptionBool", 
+	{
+		Integer,					(* uri handle *)
+		"UTF8String",				(* name *)
+		True|False 					(* value *)
+	}, 
+	"Void"
+]
+
+uriSetOptionUFT8 = LibraryFunctionLoad[$MongoLinkLib, "WL_URISetOptionUTF8", 
+	{
+		Integer,					(* uri handle *)
+		"UTF8String",				(* name *)
+		"UTF8String" 				(* value *)
+	}, 
+	"Void"
+]
+
+uriGetString = LibraryFunctionLoad[$MongoLinkLib, "WL_URIGetString", 
+	{
+		Integer						(* uri handle *)
+	}, 
+	"UTF8String"
+]
+
+uriSetPropEnum = LibraryFunctionLoad[$MongoLinkLib, "WL_URISetPropEnum", 
+	{
+		Integer,					(* uri handle *)
+		"UTF8String",				(* value *)
+		Integer 					(* selector *)
+	}, 
+	"Void"
+]
+
 (*----------------------------------------------------------------------------*)
 PackageExport["MongoURI"]
 
@@ -39,80 +82,60 @@ DefineCustomBoxes[MongoURI,
 ]];
 
 getMLE[MongoURI[uriMLE_, __]] := uriMLE;
-getMLEID[MongoCollection[uriMLE_, __]] := ManagedLibraryExpressionID[uriMLE];
+getMLEID[MongoURI[uriMLE_, __]] := ManagedLibraryExpressionID[uriMLE];
+
+MongoURI /: ToString[x_MongoURI] := 
+	CatchFailureAsMessage @ safeLibraryInvoke[uriGetString, getMLEID @ x]
 
 (*----------------------------------------------------------------------------*)
-PackageExport["MongoURIFromString"]
-PackageScope["iMongoURIFromString"]
+PackageScope["uriFromString"]
 
-(*SetUsage[URIFromString,
-"URIFromString[uri$] creates a MongoURI[$$] from a uri$ string. 
-"
-]*)
-MongoURIFromString::inv = "Invalid URI."
-
-MongoURIFromString[uri_String] := 
-	CatchFailureAsMessage @ iMongoURIFromString[uri]
-
-iMongoURIFromString[uri_String] := Module[
+uriFromString[uri_String] := Module[
 	{
 		handle = CreateManagedLibraryExpression["URI", uriMLE],
 		res
 	},
-	res = safeLibraryInvoke[uriCreate, ManagedLibraryExpressionID[handle], uri];
+	res = safeLibraryInvoke[uriCreate, getMLEID[handle], uri];
 	System`Private`SetNoEntry @ MongoURI[handle, uri]
 ]
 
 (*----------------------------------------------------------------------------*)
-(* internal Function *)
-PackageExport["MongoURIConstruct"]
+PackageScope["uriConstruct"]
 
-Options[MongoURIConstruct] = {
-	"Username" -> None,
-	"Password" -> None,
-	"Database" -> None,
-	"SSL" -> Automatic
-};
+setprop[func_, uri_, opts_, val1_, val2_] := 
+	If[KeyExistsQ[opts, val1], safeLibraryInvoke[func, getMLEID@uri, opts[val1], val2]];
 
-MongoURIConstruct[host_String, port_Integer, opts:OptionsPattern[]] := Module[
-	{
-		username = OptionValue["Username"],
-		password = OptionValue["Password"],
-		database = OptionValue["Database"],
-		ssl = OptionValue["SSL"],
-		cred, uri, tempCred
-	},
+uriConstruct[connection_String, opts_Association] := Module[
+	{uri, opts2},
+	uri = uriFromString[connection];
 
-	If[password === "$Prompt",
-		tempCred = AuthenticationDialog["UsernamePassword" -> {"Username" -> username}];
-		If[Head[tempCred] =!= Association,
-			Message[OpenMongoConnection::authcancel];
-			Return[$Failed]
-		];
-		{username, password} = Lookup[tempCred, {"Username", "Password"}];
-	];
-	
-	If[(username =!= None) && (password =!= None), 
-		cred = StringJoin[URLEncode[username], ":", URLEncode[password], "@"], 
-		cred = ""
-	];
-	If[database =!= None, 
-		database = StringJoin["/", URLEncode[database]], 
-		database = ""
-	];
+	(** Authentication **)
+	setprop[uriSetPropEnum, uri, opts, "authmechanism", 1];
+	setprop[uriSetPropEnum, uri, opts, "authsource", 2];
+	setprop[uriSetPropEnum, uri, opts, "password", 5];
+	setprop[uriSetPropEnum, uri, opts, "username", 6];
 
-	uri = StringJoin["mongodb://", cred, host, ":", ToString[port], database];
-	
-	(* ssl options: add to URI if True or False, but not Automatic *)
-	If[ssl,
-		uri = StringJoin[uri, "?ssl=true"],
-		uri = StringJoin[uri, "?ssl=false"]
-	];
-	MongoURIFromString[uri]
+	(** Compress **)
+	setprop[uriSetPropEnum, uri, opts, "compressors", 3];
+
+	(** Other **)
+	opts2 = KeyDrop[opts, {"authmechanism", "authsource", "password", "username", "compressors"}];
+	KeyValueMap[uriSetOption[uri, #1, #2]&, opts2];
+	uri
 ]
 
-MongoURIConstruct[host_String, opts:OptionsPattern[]] := 
-	MongoURIConstruct[host, 27017, opts]
+(*----------------------------------------------------------------------------*)
+PackageScope["uriSetOption"]
 
-MongoURIConstruct[opts:OptionsPattern[]] := 
-	MongoURIConstruct["localhost", 27017, opts]
+uriSetOption::invtype = "Invalid type. Only String, Integer or boolean types supported."
+
+uriSetOption[uri_MongoURI, name_String, val_Integer] := 
+	safeLibraryInvoke[uriSetOptionInt32, getMLEID @ uri, name, val]
+
+uriSetOption[uri_MongoURI, name_String, val_String] := 
+	safeLibraryInvoke[uriSetOptionUFT8, getMLEID @ uri, name, val] 
+
+uriSetOption[uri_MongoURI, name_String, val_ /; BooleanQ[val]] := 
+	safeLibraryInvoke[uriSetOptionBool, getMLEID @ uri, name, val] 
+
+uriSetOption[___] := ThrowFailure[uriSetOption::invtype]
